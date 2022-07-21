1937019321972b30e198b2790330baf8b8de22b3
Rodrigo Véliz <rvelizs@unsa.edu.pe>
2022-07-21 00:12:28 -0500

    Verificar los archivos

--- my-dream-app/src/app/app.component.ts
+++ my-dream-app/src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-dream-app';
+  name = "Rodrigo Véliz S.";
+  email = "rvelizs@unsa.edu.pe";
+  webpage = "https://www.unsa.edu.pe/"
 }
