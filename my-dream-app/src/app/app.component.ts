f2ff72f356067790777eed2253765667450430cc
Rodrigo Véliz <rvelizs@unsa.edu.pe>
2022-08-04 04:42:36 -0500

    Botones que borran elementos de una lista

--- my-dream-app/src/app/app.component.ts
+++ my-dream-app/src/app/app.component.ts
@@ -37,4 +37,12 @@ export class AppComponent {
   sayHello() {
     alert("Hola desde app.component");
   }
+
+  deleteUser(user : any) {
+    for (let i = 0; i < this.users.length; i++) {
+      if (user == this.users[i]) {
+        this.users.splice(i, 1);
+      }
+    }
+  }
 }
