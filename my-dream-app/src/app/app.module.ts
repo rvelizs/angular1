b4736b1142078c5979ffd58dd701a1bfe0f9c26f
Rodrigo Véliz <rvelizs@unsa.edu.pe>
2022-08-04 04:07:51 -0500

    Pasando datos al Componente user

--- my-dream-app/src/app/app.module.ts
+++ my-dream-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloWorldComponent
+    HelloWorldComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
